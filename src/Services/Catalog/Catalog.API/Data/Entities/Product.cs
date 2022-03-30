d9e615680a21eb061c88468ebd1dcafb86614472
ivenbews <as_lod@mail.com>
2022-03-31 03:37:12 +0500

    pages was fixed

--- src/Services/Catalog/Catalog.API/Data/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Data/Entities/Product.cs
@@ -10,6 +10,6 @@ namespace Catalog.API.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
-        public string ImageURL { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
