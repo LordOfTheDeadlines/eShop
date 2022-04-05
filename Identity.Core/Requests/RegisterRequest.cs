c175789517f0b85ad8168d64ee7bd6d55aee008f
Ekaterina <50626720+Sguhaaa@users.noreply.github.com>
2022-04-05 18:19:09 +0500

    Add requests and responses models for identity

---
+++ Identity.Core/Requests/RegisterRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Core.Requests
+{
+    internal class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Identity.Core/Responses/DataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Core.Responses
+{
+    public class DataResponse
+    {
+        public string Value { get; set; }
+    }
+}
