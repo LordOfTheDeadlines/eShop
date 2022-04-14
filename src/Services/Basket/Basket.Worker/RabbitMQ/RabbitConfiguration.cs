fb883c4cac566b347898a94970f4815ffb1930ea
ivenbews <as_lod@mail.com>
2022-04-14 18:23:32 +0500

    basket api and worker were created

---
+++ src/Services/Basket/Basket.Worker/RabbitMQ/RabbitConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Worker.RabbitMQ
+{
+    class RabbitConfiguration
+    {
+        public string Host = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+        public int Port = int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_PORT"));
+        public string User = Environment.GetEnvironmentVariable("RABBITMQ_USER");
+        public string Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
+        public int ConnectInterval = 4000;
+        public int ConnectRetries = 10;
+    }
+}
