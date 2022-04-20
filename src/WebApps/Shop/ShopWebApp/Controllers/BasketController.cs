ff94261b849491cdfc29b237fa1635bcc538418e
Ekaterina <50626720+Sguhaaa@users.noreply.github.com>
2022-04-20 13:35:49 +0500

    add getUserData method

--- src/WebApps/Shop/ShopWebApp/Controllers/BasketController.cs
+++ src/WebApps/Shop/ShopWebApp/Controllers/BasketController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShopWebApp.Models;
+using ShopWebApp.Models.Account;
 using ShopWebApp.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ShopWebApp.Controllers
@@ -20,26 +24,27 @@ namespace ShopWebApp.Controllers
         public async Task<ActionResult> Index()
         {
             var userId = HttpContext.User; 
-            return View(await _basketService.GetBasket(1));
+            return View(await _basketService.GetBasket(GetUserData().Id));
         }
 
         public async Task<ActionResult> DeleteFromBasket(int productId)
         {
-            var userName = "swn";
-            await _basketService.DeleteFromBasket(1, productId);
+            await _basketService.DeleteFromBasket(GetUserData().Id, productId);
             return RedirectToAction(nameof(Index));
         }
 
         public async Task<ActionResult> AddToBasket(int productId)
         {
-            var userName = "swn";
-            await _basketService.AddToBasket(1, productId);
+            await _basketService.AddToBasket(GetUserData().Id, productId);
             return RedirectToAction(nameof(Index));
         }
 
-        private void GetCookies()
+        private UserModel GetUserData()
         {
-            var authCookies = HttpContext.Request.Cookies["Authorization"];
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(HttpContext.Request.Cookies["Authorization"]);
+            var jti = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.UserData).Value;
+            return JsonSerializer.Deserialize<UserModel>(jti);
         }
     }
 }
--- src/WebApps/Shop/ShopWebApp/Models/Account/UserModel.cs
+++ src/WebApps/Shop/ShopWebApp/Models/Account/UserModel.cs
@@ -8,9 +8,7 @@ namespace ShopWebApp.Models.Account
 {
     public class UserModel
     {
-        [Required]
         public int Id { get; set; }
-        [Required]
         public string Username { get; set; }
         public ICollection<string> Roles { get; set; }
     }
